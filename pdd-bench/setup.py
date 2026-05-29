97f9f1694e053d7d961d37fb60a884f0fb642e47
victoriazinkovich <zinkovich.vs@phystech.edu>
2026-05-29 18:17:26 +0300

    Improve setup for easier imports

--- pdd-bench/setup.py
+++ pdd-bench/setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="traffic_signs",
+    name="pdd_bench",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(include=[
+        "envs", "envs.*",
+        "agents", "agents.*",
+        "traffic_signs", "traffic_signs.*",
+        "scripts", "scripts.*",
+    ]),
     python_requires=">=3.8",
 )
