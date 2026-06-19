272dac9bed0d0102fe7a9d38d3f3a78a851ca3d3
victoriazinkovich <zinkovich.vs@phystech.edu>
2026-06-19 13:51:26 +0300

    Fix modified idm

--- pdd-bench/scripts/per_sign_bench/run_benchmark.py
+++ pdd-bench/scripts/per_sign_bench/run_benchmark.py
@@ -42,7 +42,8 @@ if CARL_NUPLAN_DIR.exists():
 from envs.sumo_env import TrafficSignSumoEnv
 from envs.sumo_traffic_manager import SumoTrafficManager
 from envs.traffic_sign_env import TrafficSignEnv
-from metadrive.policy.idm_policy import IDMPolicy, ModifiedIDMPolicy
+from agents.policies.modified_idm_sign_compliant import ModifiedIDMSignCompliantPolicy
+from metadrive.policy.idm_policy import IDMPolicy
 from metadrive.policy.expert_policy import ExpertPolicy
 from stable_baselines3 import PPO
 from agents.policies.comprehensive_rule_expert import ComprehensiveRuleExpertPolicy
@@ -877,7 +878,7 @@ def run_one_episode(
     if policy_type == "idm":
         policy_cls = IDMPolicy
     elif policy_type == "modified_idm":
-        policy_cls = ModifiedIDMPolicy
+        policy_cls = ModifiedIDMSignCompliantPolicy
     elif policy_type == "comprehensive_rule_expert":
         policy_cls = ComprehensiveRuleExpertPolicy
     elif policy_type == "rule_compliant":
